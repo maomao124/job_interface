0331f5761a08d296ff7c236992179f9d3e57fec1
毛 <1296193245@qq.com>
2021-10-12 20:49:39 +0800

    Initial commit

---
+++ src/test/java/test.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Project name(项目名称)：作业 接口
+ * Package(包名): PACKAGE_NAME
+ * Class(类名): test
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * Date(创建日期)： 2021/10/12
+ * Time(创建时间)： 20:18
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class test
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
